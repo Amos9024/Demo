474cc29c9b65df2974ed89f6844fb6b55d1b27d9
Amos9024 <hoamos90@gmail.com>
2026-06-20 14:08:43 +0800

    Updated Code

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/goals', (req, res) => {
 });
 
 app.post('/goals', (req, res) => { 
-  let newGoal = { id: Date.now(), title: req.body.title }; 
+  let newGoal = { id: Date.now(), ...req.body }; 
   goals.push(newGoal); 
   res.status(201).json(newGoal); 
 }); 
@@ -25,7 +25,7 @@ app.put('/goals/:id', (req, res) => {
   let id = parseInt(req.params.id); 
   let index = goals.findIndex(g => g.id === id); 
   if (index !== -1) { 
-  goals[index] = { ...goals[index], title: req.body.title }; 
+  goals[index] = { ...goals[index], ...req.body }; 
   res.json(goals[index]); 
   } else { 
   res.status(404).json({ error: 'Goal not found' }); 
@@ -37,6 +37,7 @@ app.delete('/goals/:id', (req, res) => {
   goals = goals.filter(g => g.id !== id); 
   res.json({ message: 'Deleted successfully' }); 
 }); 
+
 
 app.listen(PORT, () => { 
 console.log(`Server is running at ${PORT}`); 
